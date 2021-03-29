3fe87ae403786171d97460b67b1aa393962e18c5
Kyler Harrison <harri828@purdue.edu>
2021-03-28 22:08:19 -0400

    basic io functionality

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function inputError() {
+	var btnElem = document.getElementById("btn-container");
+	var errElem = document.createElement("div");
+	errElem.innerHTML = "Ensure that you have entered valid numbers.";
+	errElem.className = "error-msg";
+	btnElem.parentNode.insertBefore(errElem, btnElem.nextSibling);
 }
 
 document.getElementById("calc-btn").addEventListener("click", () => {
@@ -16,7 +21,18 @@ document.getElementById("calc-btn").addEventListener("click", () => {
 	var activs = ["None", "Light", "Moderate", "Very", "Olympic Athlete"];
 
 	if (htFt && htIn && age && gens.includes(gen) && activs.includes(activ)) {
-		alert("all good");
+		var mainElem = document.getElementById("main-container");
+		var outMsg = document.createElement("h3");
+		outMsg.innerHTML = "You should eat:";
+		outMsg.className = "heading-text";
+		mainElem.appendChild(outMsg);
+
+		for (var i = 0; i < 10; i++) {
+			var newguy = document.createElement("p");
+			newguy.innerHTML = "wowza";
+			newguy.className = "result";
+			mainElem.appendChild(newguy);
+		}
 	} else {
 		inputError();
 	}
